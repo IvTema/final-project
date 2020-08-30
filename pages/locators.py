9e49ba584f208267be52f3cc7fda5471c4149d5b
Nameman <duft.soft@yandex.ru>
2020-08-30 22:48:32 +0300

    test_main_page.py / locators.py edited, login_page.py added

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    ENTER = (By.CSS_SELECTOR, "#login_form")
+    LOG_IN = (By.CSS_SELECTOR, "#register_form")
