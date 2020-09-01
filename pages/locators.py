997b70adf30bd8564d65bbbed70285b0b43551d3
Nameman <duft.soft@yandex.ru>
2020-09-01 18:37:18 +0300

    test_guest_can_add_product_to_basket added => base_page.py / locators.py edited, test_product_page.py / product_page.py added

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     ENTER = (By.CSS_SELECTOR, "#login_form")
     LOG_IN = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME_ON_THE_PAGE = (By.CSS_SELECTOR, ".col-sm-6.product_main>h1")
+    PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert:nth-child(1) strong")
+    PRICE_ON_PAGE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    PRICE_IN_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_in_basket(self):
+        element_btn_add = self.browser.find_element(*ProductPageLocators.ADD_BTN)
+        element_btn_add.click()
+        self.solve_quiz_and_get_code()
+
+    def should_see_product_added_message(self):
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text == \
+               self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ON_THE_PAGE).text, (
+            'Name of the product in message != name on the page')
+
+    def price_in_message_same_as_in_basket(self):
+        assert self.browser.find_element(*ProductPageLocators.PRICE_IN_MESSAGE).text == \
+               self.browser.find_element(*ProductPageLocators.PRICE_ON_PAGE).text, (
+            'Price in message != price on the page')
